96e0d08a40164f1f6768ba9c7d49fec61b432aa6
PRASHANT SINGH <ps.prashantsingh.98@gmail.com>
2023-04-09 23:35:28 +0530

    aws sign up completed with email otp verification but using custom amplify UI.

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,15 @@ import 'react-native-gesture-handler';
 import {NativeBaseProvider} from 'native-base';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NavigationContainer} from '@react-navigation/native';
-
+import {Amplify} from 'aws-amplify';
+// @ts-ignore
+import {withAuthenticator} from 'aws-amplify-react-native';
 import theme from './utils/theme';
 import RootNavigator from './appNavigator/RootNavigator';
+import Config from './aws-exports';
+// @ts-ignore
+
+Amplify.configure(Config);
 
 const App = () => {
   return (
@@ -27,4 +33,38 @@ const App = () => {
     </NativeBaseProvider>
   );
 };
-export default App;
+const signUpConfig = {
+  header: 'My customized sign up',
+  hideAllDefaults: true,
+  signUpFields: [
+    {
+      label: 'Full Name',
+      key: 'name',
+      required: true,
+      displayOrder: 1,
+      type: 'string',
+    },
+    {
+      label: 'Email',
+      key: 'email',
+      required: true,
+      displayOrder: 2,
+      type: 'string',
+    },
+    {
+      label: 'Username',
+      key: 'username',
+      required: true,
+      displayOrder: 3,
+      type: 'string',
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      displayOrder: 4,
+      type: 'string',
+    },
+  ],
+};
+export default withAuthenticator(App, {signUpConfig});
